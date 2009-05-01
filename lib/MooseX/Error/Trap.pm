259e763e7c2a6a752e6d1b99b3fb7ea4b7b5c8cd
ben hengst <notbenh@cpan.org>
2009-05-01 14:44:30 -0700

    lets clean things up a bit for publication

--- lib/MooseX/Error/Trap.pm
+++ lib/MooseX/Error/Trap.pm
@@ -1,46 +1,52 @@
 package MooseX::Error::Trap;
-
-our $VERSION = '0.01';
 use Moose;
 use Moose::Exporter;
 Moose::Exporter->setup_import_methods(
    with_caller => [qw{trap}],
 );
 
-sub ____register_trap_dispatch {
-   my ($caller,$trap_method,$dispatch_method) = @_;
-   my $meta   = Class::MOP::Class->initialize($caller);
-   #---------------------------------------------------------------------------
-   #  Make sure that we have a storage location for trap_dispatch
-   #---------------------------------------------------------------------------
-   unless ( $meta->find_attribute_by_name('trap_dispatch') ) {
-      $meta->add_attribute(
-               'trap_dispatch',
-               is => 'rw',
-               isa => 'HashRef',
-               default => sub{{}},
-      );
+
+
+=head1 NAME
+
+MooseX::Error::Trap - The great new MooseX::Error::Trap!
+
+=head1 VERSION
+
+Version 0.00_01
+
+=cut
+
+our $VERSION = '0.00_01';
+
+
+=head1 SYNOPSIS
+
+Allows you to wrap any method in an eval and specify a dispatch method if the eval trips.
+
+   package My::Test;
+   use Moose;
+   use MooseX::Error::Trap;
+
+   trap 'some_method', 'what_to_do';
+   sub some_method {
+      ...
    }
 
-use Data::Dumper;
-
-   if ( $meta->find_attribute_by_name('trap_dispatch')->has_write_method ) {
-      #THIS IS RATHER FORECFUL!!
-      $meta->find_attribute_by_name('trap_dispatch')->set_value($meta,{ 
-         grep{ defined} 
-            #$meta->find_attribute_by_name('trap_dispatch')->get_value($meta) || undef
-            $trap_method => $dispatch_method,
-      });
-
-
-   }
-   else {
-      die 'Was not able to set the proper dispatcher as requested, likely trap_dispatch is set to "ro".';
+   sub what_to_do {
+      my ($self, $error) = @_;
+      ...
    }
 
-   
-   
-}
+=head1 Keywords
+
+=head2 trap
+
+   trap 'run_method_name', 'error_handling_method_name';
+
+Will wrap any calls to 'run_method_name' in an eval, currently you can only have one deligation method.
+
+=cut
 
 sub trap {
    my ($caller,$trap_method,$dispatch_method) = @_;
@@ -73,63 +79,13 @@ sub trap {
    );
 }
    
+=head1 TODO
 
-no MooseX::Error::Trap;
+=over 4
 
-1;
-__END__
+=item * A way to modify the relationship between the method run and the deferment method at runtime.
 
-
-
-
-
-use warnings;
-use strict;
-
-=head1 NAME
-
-MooseX::Error::Trap - The great new MooseX::Error::Trap!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use MooseX::Error::Trap;
-
-    my $foo = MooseX::Error::Trap->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=back 4
 
 =head1 AUTHOR
 
@@ -140,9 +96,6 @@ NOTBENH, C<< <NOTBENH at cpan.org> >>
 Please report any bugs or feature requests to C<bug-moosex-error-trap at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-Error-Trap>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
@@ -176,6 +129,7 @@ L<http://search.cpan.org/dist/MooseX-Error-Trap/>
 
 =head1 ACKNOWLEDGEMENTS
 
+Thanks again to team Moose.
 
 =head1 COPYRIGHT & LICENSE
 
@@ -187,4 +141,5 @@ under the same terms as Perl itself.
 
 =cut
 
+no MooseX::Error::Trap;
 1; # End of MooseX::Error::Trap
